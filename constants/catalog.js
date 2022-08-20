b32fda35d7a3f3d2ab3baafaef34f8d5a33ff82b
Arsen Shahbazyan <60728417+arsshahbazyan@users.noreply.github.com>
2022-08-20 11:16:15 +0400

    Added to project

---
+++ constants/catalog.js
@@ -0,0 +1,68 @@
+const CATALOG = [
+    {
+        id: 'el1',
+        name: 'George Orwell - 1984',
+        img: 'https://i.ibb.co/0KNgq7p/1984.jpg',
+        price: 3.00,
+    },
+    {
+        id: 'el2',
+        name: 'Homer - Iliad and Odyssey',
+        img: 'https://i.ibb.co/nBnRnnn/Iliadand-Odyssey.jpg',
+        price: 4.00,
+    },
+    {
+        id: 'el3',
+        name: 'Marcel Proust - In Search of Lost Time. Part 1',
+        img: 'https://i.ibb.co/hyJNsDW/In-Searchof-Lost-Time1.jpg',
+        price: 8.00,
+    },
+    {
+        id: 'el4',
+        name: 'Vladimir Nabokov - Invitation to a Beheading',
+        img: 'https://i.ibb.co/vV6XqKL/Invitationtoa-Beheading.jpg',
+        price: 3.99,
+    },
+    {
+        id: 'el5',
+        name: 'Gabriel Garcia Marquez - The Autumn of the Patriarch',
+        img: 'https://i.ibb.co/WGRxdQK/The-Autumnofthe-Patriarch.jpg',
+        price: 4.5,
+    },
+    {
+        id: 'el6',
+        name: 'Dante Alighieri - The Divine Comedy',
+        img: 'https://i.ibb.co/TtCzBxX/The-Divine-Comedy.jpg',
+        price: 11.00,
+    },
+    {
+        id: 'el7',
+        name: 'Mariam Petrosyan - The Gray House',
+        img: 'https://i.ibb.co/88gFsgL/The-Gray-House.jpg',
+        price: 3.5,
+    },
+    {
+        id: 'el8',
+        name: 'J. R. R. Tolkien - The Silmarillion',
+        img: 'https://i.ibb.co/mCCHZqX/The-Silmarillion.jpg',
+        price: 19.99,
+    },
+    {
+        id: 'el9',
+        name: 'Albert Camus - The Stranger',
+        img: 'https://i.ibb.co/sbT2GWb/The-Stranger.jpg',
+        price: 7.00,
+    },
+    {
+        id: 'el10',
+        name: 'Virginia Woolf - To the Lighthouse',
+        img: 'https://i.ibb.co/LtvH0mF/Tothe-Lighthouse.jpg',
+        price: 7.45,
+    },
+    {
+        id: 'el11',
+        name: 'James Joyce - Ulysses',
+        img: 'https://i.ibb.co/N62026w/Ulysses.jpg',
+        price: 9.99,
+    },
+];
